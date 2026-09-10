5c5e56928b6470a2b470ef9262b00968f0d67bf7
Luiz Lozano <luiz.machi@sp.senai.br>
2026-09-09 21:43:39 -0300

    aula 06

--- src/main/java/com/example/imagemPecas/application/images/ImagesController.java
+++ src/main/java/com/example/imagemPecas/application/images/ImagesController.java
@@ -5,15 +5,15 @@ import com.example.imagemPecas.domain.enums.ImageExtension;
 import com.example.imagemPecas.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -32,16 +33,32 @@ public class ImagesController {
             ) throws IOException
     {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",",tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueof(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
-        service.save(image);
+        URI imageUri = buildImageURL(savedImage);
+        return ResponseEntity.created(imageUri).build();
+    }
 
-        return ResponseEntity.ok().build();
+    // /v1/images/{id}
+    @GetMapping("{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id){
+        var possibleImage = service.getById(id);
+        if(possibleImage.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var image = possibleImage.get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        headers.setContentDispositionFormData("", image.getName()
+                .concat("").concat(image.getExtension().name()));
+    }
+
+    //localhost:8080/v1/images   /xzxzxzxzxzxzxzxzxzxz
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
     }
 }
--- src/main/java/com/example/imagemPecas/domain/service/ImageService.java
+++ src/main/java/com/example/imagemPecas/domain/service/ImageService.java
@@ -2,6 +2,10 @@ package com.example.imagemPecas.domain.service;
 
 import com.example.imagemPecas.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save (Image image);
+
+    Optional<Image> getById(String id);
 }
